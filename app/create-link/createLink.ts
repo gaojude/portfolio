ab9cf47543332b2d57d56baee492fb1501745c9e
Jude Gao <gaojude@outlook.com>
2024-02-19 20:26:42 -0500

    fix type

--- app/create-link/createLink.ts
+++ app/create-link/createLink.ts
@@ -3,14 +3,27 @@
 import { prisma } from "@/app/prisma";
 import { revalidatePath } from "next/cache";
 
-export async function createLink(formData) {
+export async function createLink(formData: FormData) {
   const rawFormData = {
     url: formData.get("url"),
     title: formData.get("title"),
     description: formData.get("description"),
-  };
+  } as const;
+
+  if (!rawFormData.url) {
+    throw new Error("url is null");
+  }
+
+  const url = rawFormData.url as string;
+  const title = rawFormData.title as string | null;
+  const description = rawFormData.description as string | null;
+
   await prisma.link.create({
-    data: rawFormData,
+    data: {
+      url,
+      title,
+      description,
+    },
   });
   // TODO: reset form / redirect to links page
   revalidatePath("/create-link");
