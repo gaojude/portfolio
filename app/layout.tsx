d3edd694e47eb51126af3b399b848771dc3ddb10
Jude Gao <judegao@judes-laptop.home>
2024-04-27 01:06:12 -0400

    fix shell

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,38 +1,45 @@
 import { ReactNode } from "react";
 import "./globals.css";
-import { HomeIcon, PlusIcon, Trash2Icon } from "lucide-react";
+import { MountainIcon, PlusIcon, Trash2Icon } from "lucide-react";
 import Link from "next/link";
 
 const Layout = ({ children }: { children: ReactNode }) => {
   return (
-    <>
-      <header className="flex h-16 shrink-0 items-center justify-between border-b px-4 md:px-6">
-        <nav className="flex items-center gap-6 text-lg font-medium md:text-sm lg:gap-8">
-          <Link
-            className="flex items-center gap-2 text-lg font-semibold md:text-base"
-            href="/"
-          >
-            <HomeIcon className="h-6 w-6" />
-            Home
-          </Link>
-          <Link
-            className="flex items-center gap-2 text-lg font-semibold md:text-base"
-            href="/create"
-          >
-            <PlusIcon className="h-6 w-6" />
-            Create
-          </Link>
-          <Link
-            className="flex items-center gap-2 text-lg font-semibold md:text-base"
-            href="/?delete=true"
-          >
-            <Trash2Icon className="h-6 w-6" />
-            Delete
-          </Link>
-        </nav>
-      </header>
-      {children}
-    </>
+    <html lang="en">
+      <head>
+        <meta charSet="UTF-8" />
+        <title>Jude&apos;s Bookmarks</title>
+      </head>
+      <body>
+        <header className="flex h-16 shrink-0 items-center justify-between border-b px-4 md:px-6">
+          <nav className="flex items-center gap-6 text-lg font-medium md:text-sm lg:gap-8">
+            <Link
+              className="flex items-center gap-2 text-lg font-semibold md:text-base"
+              href="/"
+            >
+              <MountainIcon className="h-6 w-6" />
+              <span>Jude&apos;s Bookmarks</span>
+            </Link>
+
+            <Link
+              className="flex items-center gap-2 text-lg font-semibold md:text-base"
+              href="/create"
+            >
+              <PlusIcon className="h-6 w-6" />
+              Create
+            </Link>
+            <Link
+              className="flex items-center gap-2 text-lg font-semibold md:text-base"
+              href="/?delete=true"
+            >
+              <Trash2Icon className="h-6 w-6" />
+              Delete
+            </Link>
+          </nav>
+        </header>
+        {children}
+      </body>
+    </html>
   );
 };
 
